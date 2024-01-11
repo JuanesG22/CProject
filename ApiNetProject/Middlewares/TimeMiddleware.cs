62163e53105014f304ce4f81cdfacd2ae9f95089
JuanesG22 <102272125+JuanesG22@users.noreply.github.com>
2024-01-11 09:24:15 -0500

    rest

---
+++ ApiNetProject/Middlewares/TimeMiddleware.cs
@@ -0,0 +1,21 @@
+public class TimeMiddleware
+{
+    readonly RequestDelegate next;
+    public TimeMiddleware(RequestDelegate nextRequest)
+    {
+        next = nextRequest;
+    }
+    public async Task Invoke(Microsoft.AspNetCore.Http.HttpContext context)
+    {
+        await next(context);
+
+        if(context.Request.Query.Any(p=> p.Key == "time"))
+        {
+            await context.Response.WriteAsync(DateTime.Now.ToShortDateString());
+        }
+    }
+    public static class TimeMiddlewareExtension
+    {
+        
+    }
+}
